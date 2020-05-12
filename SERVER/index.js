fa09e05ba2532412fa214ea3cec816ce77f404a0
raul-garces <raulgarces.fare@outlook.com>
2020-05-12 04:59:24 -0500

    commit 3

--- SERVER/index.js
+++ SERVER/index.js
@@ -7,11 +7,13 @@ const path = require("path");
 
 //setings
 
+//ddsfdsf
+
 app.set('appName', "search house");
 app.set("views", path.join(__dirname, "views"))
 app.set('port', process.env.PORT || 3000);
 app.engine('.hbs', exphbs({
-    defaultLayout: "main",
+    defaultLayout: "main", 
     layoutsDir: path.join(app.get("views"), "layouts"),
     partialsDir: path.join(app.get("views"), "partials"),
     extname:".hbs",
