3c988b04e573c7dfad1d003e9b8d0c3b2354ed4b
raul-garces <raulgarces.fare@outlook.com>
2020-05-12 05:44:08 -0500

    se corrijio fallo del servidor

--- SERVER/index.js
+++ SERVER/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
+const exphbs = require('express');
+const path = require("path");
 
 
 //setings
@@ -9,17 +11,18 @@ const app = express();
 
 app.set('appName', "search house");
 
-app.set("views", path.join(__dirname, "views"))
+
 app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, "views"));
 app.engine('.hbs', exphbs({
-    defaultLayout: "main", 
+    defaultLayout: "main",
     layoutsDir: path.join(app.get("views"), "layouts"),
     partialsDir: path.join(app.get("views"), "partials"),
-    extname:".hbs",
+    extname: ".hbs",
     helpers: require('./lib/handlebars')
 }));
 app.set("view engine", ".hbs");
-app.set(express.urlencoded({extended: true}));
+app.set(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.set('port', process.env.PORT || 5000);
@@ -31,14 +34,19 @@ app.set('view engine', "ejs");
 //mideweres 
 app.use(express.json());
 app.use(morgan('dev'));
+app.use((req, res, next) => {
+    next();
+});
 
 
 
 //rutes
 app.use(require("./routes/index.route.js"));
+app.use('/links', require("./routes/links"));
+app.use(require("./routes/autentication"));
 
-
-
+//public
+app.use(express.static(path.join(__dirname, "public")));
 
 
 app.listen(app.get("port"), () => {
